d1a59e4bbb0f879dc51015c4c158cda12de92562
Szilagyi Zsolt <84618585+Zsolt-Szilagyi92@users.noreply.github.com>
2021-11-08 19:26:12 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('star-icon').onclick = changeColor;   
+
+    function changeColor() {
+        document.getElementsByClassName('star-icon').style.color = "#ffc107";
+        return false;
+    }   
